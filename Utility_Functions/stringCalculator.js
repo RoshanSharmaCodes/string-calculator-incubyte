d79f4577f6e7371557d031ed6620d943ea248ed3
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-07-20 22:07:05 +0530

    Code to pass TC1: Basic functionality.

--- Utility_Functions/stringCalculator.js
+++ Utility_Functions/stringCalculator.js
@@ -1,4 +1,22 @@
 class StringCalculator {
+    add(numbers) {
+        //Edge cases.
+        if (!numbers) return 0
+        if (typeof numbers !== "string") return 0
+    
+        //Declaration space.
+        let delimiter = ","
+        let numString = numbers
+    
+        //Converting number strings to number array.
+        const numArray = numString
+          .split(delimiter)
+          .map((val) => parseInt(val, 10))
+          .filter((val) => !isNaN(val))
+    
+        //Calculate the sum of number array.
+        return numArray.reduce((sum, num) => sum + num, 0)
+      }
     
 }
 
