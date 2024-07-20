d6f1a23e64099c986f103ce7006a05a9f7180898
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-07-20 22:12:30 +0530

    TC2:  New line delimiter test-cases.

--- Testing/Utility.test.js
+++ Testing/Utility.test.js
@@ -3,7 +3,7 @@ import { describe, test, expect } from "vitest"
 
 describe("String Calculator Functionality", () => {
   const strCalc = new StringCalculator()
-  
+
   describe("1. Basic Functionality Testing :-", () => {
     test("TC: 1.1 -> Return 0 for Empty String.", () => {
       expect(strCalc.add("")).toBe(0)
@@ -28,6 +28,19 @@ describe("String Calculator Functionality", () => {
 
     test("TC: 1.6 -> Checking for the type of output to be number", () => {
       expect(strCalc.add("1,3,5,6")).toBeTypeOf("number")
+    })
+  })
+
+  
+  describe("2. New Line as Delimiter :-", () => {
+    test("TC: 2.1 -> Handle new lines between numbers.", () => {
+      expect(strCalc.add("1\n2,3")).toBe(6)
+    })
+
+    test("TC: 2.2 -> Handle multiple new lines between numbers.", () => {
+      expect(strCalc.add("1\n2,3\n5")).toBe(11)
+      expect(strCalc.add("1\n2,3\n5\n6\n7")).toBe(24)
+      expect(strCalc.add("1\n2,\n3\n5\n\n6\n7")).toBe(24)
     })
   })
 })
